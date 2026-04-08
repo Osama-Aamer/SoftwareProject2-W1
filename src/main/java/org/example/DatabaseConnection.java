8e7a1f5a851ca9e1b4e63d13ee3ca821630dd808
Osama-Aamer <osamaaamer2@gmail.com>
2026-04-08 23:55:23 +0300

    configure build error in jenkins

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mariadb://localhost:3306/shopping_cart_localization";
     private static final String USER = "root";
-    private static final String PASS = "password";
+    private static final String PASS = "";  // blank = no password (MariaDB default)
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
